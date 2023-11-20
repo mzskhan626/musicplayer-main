ccd1d19d73dd9c05cc654743d580e741d420c240
mzskhan <mzskhan626@gmail.com>
2023-11-19 19:55:53 -0800

    This application allow user to change the song, fast forward and rewinding the song.

---
+++ README.md
@@ -0,0 +1,3 @@
+# musicPlayer
+
+#### This application allow user to change the song, fast forward and rewinding the song.
---
+++ script.js
@@ -0,0 +1,122 @@
+const image = document.getElementById("cover"),
+  title = document.getElementById("music-title"),
+  artist = document.getElementById("music-artist"),
+  currentTimeEl = document.getElementById("current-time"),
+  durationEl = document.getElementById("duration"),
+  progress = document.getElementById("progress"),
+  playerProgress = document.getElementById("player-progress"),
+  prevBtn = document.getElementById("prev"),
+  nextBtn = document.getElementById("next"),
+  playBtn = document.getElementById("play"),
+  container = document.querySelector(".container"),
+  phoneBtn = document.querySelector(".phone-play");
+
+// background = document.getElementById("bg-img");
+
+const music = new Audio();
+
+const songs = [
+  {
+    path: "images/2.mp3",
+    displayName: "Countin' On You",
+    cover: "images/1.jpg",
+    artist: "Fast X",
+  },
+  {
+    path: "images/1.mp3",
+    displayName: "Champagne Poetry",
+    cover: "images/2.jpg",
+    artist: "Drake",
+  },
+  {
+    path: "images/3.mp3",
+    displayName: "All My Life",
+    cover: "images/3.jpg",
+    artist: "Lil Durk",
+  },
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay() {
+  if (isPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+}
+
+function playMusic() {
+  isPlaying = true;
+  // Change play button icon
+  playBtn.classList.replace("fa-play", "fa-pause");
+  // Set button hover title
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+function pauseMusic() {
+  isPlaying = false;
+  // Change pause button icon
+  playBtn.classList.replace("fa-pause", "fa-play");
+  // Set button hover title
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+function loadMusic(song) {
+  music.src = song.path;
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  image.src = song.cover;
+  // background.src = song.cover;
+}
+
+function changeMusic(direction) {
+  musicIndex = (musicIndex + direction + songs.length) % songs.length;
+  loadMusic(songs[musicIndex]);
+  playMusic();
+}
+
+function updateProgressBar() {
+  const { duration, currentTime } = music;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+
+  const formatTime = (time) => String(Math.floor(time)).padStart(2, "0");
+  durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(
+    duration % 60
+  )}`;
+
+  currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(
+    currentTime % 60
+  )}`;
+}
+
+function setProgressBar(e) {
+  const width = playerProgress.clientWidth;
+  const clickX = e.offsetX;
+  music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener("click", togglePlay);
+prevBtn.addEventListener("click", () => changeMusic(-1));
+nextBtn.addEventListener("click", () => changeMusic(1));
+music.addEventListener("ended", () => changeMusic(1));
+music.addEventListener("timeupdate", updateProgressBar);
+playerProgress.addEventListener("click", setProgressBar);
+
+loadMusic(songs[musicIndex]);
+
+//Scroll Reveal
+
+ScrollReveal().reveal(".one", { delay: 1200 });
+ScrollReveal().reveal(".two", { delay: 1700 });
+// ScrollReveal().reveal(".container", { delay: 900 });
+
+//Phone Play Button
+
+phoneBtn.addEventListener("click", function () {
+  container.classList.toggle("show");
+});
